7f85e14cdb27b73fa817f93efeea7a1869164795
Geizielder <geizielder.gervazio@gmail.com>
2022-12-01 20:35:30 -0300

    Caturando eventos do teclado e tratamento de erro

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -1,17 +1,31 @@
 function tocaSom(idElementoAudio) {
-  document.querySelector(idElementoAudio).play();
-} 
+    const elemento = document.querySelector(idElementoAudio);
+
+    if (elemento  && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+}
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-
-for(let i = 0; i < listaDeTeclas.length; i++ ) {
+for (let i = 0; i < listaDeTeclas.length; i++) {
     const tecla = listaDeTeclas[i]
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`
-    tecla.onclick = function() {
+    tecla.onclick = function () {
         tocaSom(idAudio);
     }
-    
-}
 
+    tecla.onkeydown = function (event) {
+        console.log(event.code);
+        if (event.code === 'Space' || event.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
+}
